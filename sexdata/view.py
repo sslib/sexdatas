71343c850a9e851a37f9b669416516e1f2007f41
皇珠子 <37493409+sslib@users.noreply.github.com>
2018-05-16 20:51:25 +0800

    Update view.py

--- sexdata/view.py
+++ sexdata/view.py
@@ -9,15 +9,25 @@ contexts['sa'] = '山岸逢花'
 #zzzzzz
 
 def hello(request):
-    # return HttpResponse("hello sexdata")
     return render(request, 'index.html', contexts)
 def qb(request):
-    # return HttpResponse("hello sexdata")
     return render(request, 'qb.html', contexts)
 def sa(request):
-    # return HttpResponse("hello sexdata")
     return render(request, 'sa.html', contexts)
 def signup(request):
     return HttpResponse("signup")
 def signin(request):
     return HttpResponse("signin")
+def actress(request):
+    return HttpResponse("actress")
+def firm(request):
+    return HttpResponse("firm")
+def sort(request):
+    return HttpResponse("sort")
+def sale(request):
+    return HttpResponse("sale")
+def rank(request):
+    return HttpResponse("rank")
+def api(request):
+    return HttpResponse("api")
+
